cfcc700c9644c3bd768aae1f952f9879ac530fc2
Salavat <altinbaevsal@gmail.com>
2022-01-18 22:41:22 +0300

    add create and home forms

--- cities/views.py
+++ cities/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView
 
+from cities.forms import CityForm
 from cities.models import City
 
 
 def home(request, pk=None):
     if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
         print(request.POST)
     if pk:
         city = get_object_or_404(City, id=pk)
         context = {'object': city}
         return render(request, 'cities/detail.html', context)
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    context = {'objects_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
