9695799e8817f83cfdf3530d4ed6188294128621
Salavat <altinbaevsal@gmail.com>
2022-01-19 19:57:14 +0300

    [ADD] Deletable of data on the homepage

--- cities/views.py
+++ cities/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView, UpdateView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
 from cities.forms import CityForm
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView', 'CityCreateView','CityUpdateView'
+    'home', 'CityDetailView', 'CityCreateView', 'CityUpdateView', 'CityDeleteView'
 )
 
 def home(request, pk=None):
@@ -42,4 +42,10 @@ class CityUpdateView(UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
     success_url = reverse_lazy('cities:home')
