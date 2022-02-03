641cc73c97a61018f0526da791ff28147595dd07
Salavat <altinbaevsal@gmail.com>
2022-02-03 13:46:48 +0300

    [ADD] train's view and detail page

--- diplom/urls.py
+++ diplom/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("cities/", include(("cities.urls", "cities"))),
+    path("trains/", include(("trains.urls", "trains"))),
     path("about/", about),
 ]
